fd84c300a794a09c201f3789c29c20f14f8c8ad9
melissa-martell <216030929+melissa-martell@users.noreply.github.com>
2025-12-13 11:38:15 -0500

    Add helpers librery

---
+++ helpers.py
@@ -0,0 +1,27 @@
+def validate_input(dicom_file):
+    error_msg = None
+
+    if dicom_file.filename == '':
+        error_msg = "Please select a DICOM file"
+    elif not dicom_file.filename.lower().endswith('.dcm'):
+        error_msg = "File must be .dcm format"
+    
+    return error_msg
+
+def anonymize_dicom(dicom):
+     # Lista de tags a anonimizar (códigos DICOM)
+    tags_to_remove = [
+        (0x0010, 0x0010),  # Patient Name
+        (0x0010, 0x0020),  # Patient ID
+        (0x0010, 0x0030),  # Patient Birth Date
+        (0x0010, 0x0040),  # Patient Sex
+        (0x0008, 0x0080),  # Institution Name
+        (0x0008, 0x0090),  # Referring Physician
+        (0x0008, 0x1050),  # Performing Physician
+        (0x0008, 0x0081),  # Institution Address
+    ]
+    for tag in tags_to_remove:
+        if tag in dicom.dicom_file:
+            dicom.dicom_file[tag].value = 'ANONYMOUS'
+    print("Datos del paciente anonimizados")
+    return True
